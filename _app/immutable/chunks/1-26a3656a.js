8820218bee4f238bd49b8e2936934f66bd06e208
berkay <66202981+berkay-yalin@users.noreply.github.com>
2022-06-07 17:27:29 +0100

    build @ 17:27:29 07.06.2022

---
+++ _app/immutable/chunks/1-26a3656a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-fb07ef3f.js";export{t as component};
---
+++ _app/immutable/chunks/2-c242a711.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-3a852ca6.js";export{t as component};
