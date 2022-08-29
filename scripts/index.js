c89f53db4af2cd8d30abb027bec0bd038142b4a1
berkay <66202981+berkay-yalin@users.noreply.github.com>
2022-08-29 07:11:13 +0100

    index.html

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+document.getElementById('p2pImage').addEventListener('click', function () {
+  window.location.href = "./psuedocode2python.html";
+});
+
+document.getElementById('piImage').addEventListener('click', function () {
+  window.location.href = "./pythonInterpreter.html";
+});
+
+document.getElementById('ghImage').addEventListener('click', function () {
+  window.location.href = "https://github.com/berkay-yalin";
+});
