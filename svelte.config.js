1c676021a738a1152beca672b91dcd8b801c7ebb
berkay <66202981+berkay-yalin@users.noreply.github.com>
2022-10-16 01:12:13 +0100

    initial commit #2 (with typescript)

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: static_adapter()
+		adapter: adapter()
 	}
 };
 
