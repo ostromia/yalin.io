0bf013dd63c74ee277073c1773ad597aca6d2ef8
berkay <66202981+berkay-yalin@users.noreply.github.com>
2022-10-19 03:27:50 +0100

    delete _old_stuff

--- _old_stuff/onMount.js
+++
@@ -1,9 +0,0 @@
-import {onMount} from 'svelte';
-let innerHeight: number;
-let innerWidth: number;
-onMount(() => {
-  innerHeight = window.innerHeight;
-  innerWidth = window.innerWidth;
-  console.log(innerWidth);
-  console.log(innerHeight);
-});
