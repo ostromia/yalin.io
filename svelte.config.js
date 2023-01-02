c770f2e840b97e21045fb915512f4bd09f998092
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-01-02 23:47:13 +0000

    upgraded to sveltekit 1.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+	// // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// // for more information about preprocessors
+	// preprocess: vitePreprocess(),
+
 	preprocess: preprocess({
-    scss: {
-      prependData: `@import './src/styles.scss';`
-    }
-  }),
+		scss: {
+			prependData: `@import './src/styles.scss';`
+		}
+	}),
+
 
 	kit: {
 		adapter: adapter()
