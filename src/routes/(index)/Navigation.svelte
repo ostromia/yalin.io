ed61f3f4f8c21b139a56cf919cc94e7d7476160f
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-04-26 22:20:56 +0100

    fix(index): nav font sizes

--- src/routes/(index)/Navigation.svelte
+++ src/routes/(index)/Navigation.svelte
@@ -5,66 +5,59 @@
 </script>
 
 <nav>
-	<a class="nav-text" id="nav-index" href="{base}/">
+	<a class="first" href="{base}/">
 		~/berkay-yalin.io
 	</a>
 
-	<a class="nav-text" style="grid-area: projects" href="{base}/projects">
+	<a class="text" href="{base}/projects">
 		Projects
 	</a>
 
-	<a class="nav-text" style="grid-area: gists" href="https://gist.github.com/berkay-yalin">
+	<a class="text" href="https://gist.github.com/berkay-yalin">
 		Gists
 	</a>
 
-	<a class="nav-image" style="grid-area: github" href="https://github.com/berkay-yalin">
+	<a class="last" href="https://github.com/berkay-yalin">
 		<img alt="" src={github_devicon}>
 	</a>
 </nav>
 
 <style lang="scss">
 	nav {
-		height: 5vh;
-		width: 100%;
-
-		font-family: "JetBrains Mono";
+		height: 2rem;
 		background-color: $fc;
-
-		padding: 0 1rem 0 1rem;
-		box-sizing: border-box;
 
 		display: grid;
 		gap: 1rem;
 		grid-template-columns: repeat(3, min-content) auto min-content;
-		grid-template-rows: 1fr;
-		grid-template-areas: "index projects gists . github";
+		grid-template-areas: "first a0 a1 . last";
 	}
 
-	#nav-index {
-		grid-area: index;
-		padding-right: 1vw;
-		font-size: 2vh;
-	}
-
-	.nav-text {
+	nav > a {
+		font-family: "JetBrains Mono";
 		white-space: nowrap;
-
 		color: white;
 		text-decoration: none;
-
-		font-size: 1.5vh;
-		line-height: 5vh;
-		font-weight: 500;
+		line-height: 2rem;
 	}
 
-	.nav-image {
+	.first, .last {
+		padding: 0 1rem;
+	}
+
+	.text {
+		font-size: 0.8rem;
+	}
+
+	.last {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		grid-area: last;
 
 		> img {
-			height: 3vh;
-			width: 3vh;
+			height: 1.5rem;
+			width: 1.5rem;
 		}
 	}
 </style>
