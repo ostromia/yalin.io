85f2ee0f309cbcc14cd36dc45e7975272312032b
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-04-27 15:06:45 +0100

    fix(index): scrollable div below fixed navbar

--- src/routes/(index)/+layout.svelte
+++ src/routes/(index)/+layout.svelte
@@ -2,34 +2,15 @@
 	import Navigation from '$index/Navigation.svelte';
 </script>
 
-<svelte:head>
-	<style>
-		::-webkit-scrollbar {
-			width: 0.5vw;
-		}
-		::-webkit-scrollbar-track {
-			background: rgb(206, 206, 206);
-		}
-		::-webkit-scrollbar-thumb {
-			background: rgb(136, 136, 136);
-		}
-		::-webkit-scrollbar-thumb:hover {
-			background: rgb(85, 85, 85);
-		}
-	</style>
-</svelte:head>
-
 <Navigation/>
-<div id="main-wrapper">
-	<slot/>
-</div>
+<main><slot/></main>
 
 <style lang="scss">
-	:global(body) {
+	main {
+		position: fixed;
+		inset: 2rem 0 0 0;
+		overflow-y: auto;
 		overflow-x: hidden;
-	}
-
-	#main-wrapper {
 		padding: 2rem;
 		box-sizing: border-box;
 	}
