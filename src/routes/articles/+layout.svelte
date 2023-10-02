c4d674012283480e94272528337b73fea6d53709
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-10-02 16:31:04 +0100

    feat: knight-probability-in-chessboard

---
+++ src/routes/articles/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Navigation from '$r/(index)/Navigation.svelte';
+</script>
+
+<Navigation/>
+<main><slot/></main>
+
+<style lang="scss">
+	main {
+		position: fixed;
+		inset: 2rem 0 0 0;
+		overflow-y: auto;
+		overflow-x: hidden;
+		padding: 2rem;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/articles/knight-probability-in-chessboard/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	import "@fontsource/albert-sans";
+
+	import Problem from "$r/articles/knight-probability-in-chessboard/Problem.svelte"
+	import Solution from "$r/articles/knight-probability-in-chessboard/Solution.svelte"
+</script>
+
+<main>
+	<h1>Leet Code 688. Knight Probability in Chessboard</h1>
+	<Problem/>
+	<Solution/>
+</main>
+
+<style lang="scss">
+	:global(html) {
+		background-color: $bc;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 0rem 7rem;
+	}
+	h1 {
+		margin: 0;
+		padding: 1rem;
+		box-sizing: border-box;
+		font-family: "Albert Sans";
+		color: white;
+	}
+</style>
