f99980d4a000ddb9d938868b265ed786270bba32
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-10-13 06:07:50 +0100

    feat(articles): makeshift articles page
    incomplete webpage only built to provide links to articles and other webpages

---
+++ src/routes/(index)/articles/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<p>this webpage is currently under construction</p>
+
+<ul>
+    <li>
+        <a href="{base}/articles/knight-probability-in-chessboard">
+            Leet Code 688. Knight probability in chessboard
+        </a>
+    </li>
+
+    <li>
+        <a href="{base}/articles/gamma-functions">
+            Gamma Functions
+        </a>
+    </li>
+
+    <li>
+        <a href="https://gist.github.com/berkay-yalin/e6f737f32db49f72f30dde1b8fdf03ce">
+            Vim Indentation Configurations
+        </a>
+    </li>
+</ul>
+
+<style lang="scss">
+    p {
+        color: white;
+    }
+
+    a {
+        color: rgb(88, 166, 255);
+    }
+</style>
