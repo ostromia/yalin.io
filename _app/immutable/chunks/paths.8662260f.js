299f6d65e7824242856bd348b75df8a10145783c
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-10-13 08:16:18 +0100

    Fri 13 Oct 2023 08:16:18 AM BST

---
+++ _app/immutable/chunks/paths.8662260f.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_8lcrw3)==null?void 0:s.base)??"/berkay-yalin.io";var a;const l=((a=globalThis.__sveltekit_8lcrw3)==null?void 0:a.assets)??e;export{l as a,e as b};
