30cbba5c746047d06909cfd5e1557aa23fd339ac
berkay <66202981+berkay-yalin@users.noreply.github.com>
2024-01-24 14:12:59 +0000

    feat(articles): redesign articles page

--- src/routes/(index)/articles/+page.svelte
+++ src/routes/(index)/articles/+page.svelte
@@ -1,35 +1,107 @@
 <script>
-	import { base } from '$app/paths';
+    import { base } from "$app/paths";
+
+	import "@fontsource/jetbrains-mono";
+	import "@fontsource/albert-sans";
+
+    const sections = [
+        {
+            heading: "Articles",
+            description: "Articles I've written on topics that interest me:",
+            list: [
+                {
+                    url: `${base}/articles/knight-probability-in-chessboard`,
+                    label: "Leet Code 688. Knight probability in chessboard",
+                    description: "A much more efficient solution to Leet Code question 688 using recursion and the Law of Total Probability"
+                },
+            ]
+        },
+        {
+            heading: "Resources",
+            description: "Useful resources I've collected from around the internet:",
+            list: [
+                {
+                    url: `${base}/articles/gamma-functions`,
+                    label: "Gamma Functions",
+                    description: "Definitions of the gamma functions"
+                },
+                {
+                    url: `${base}/articles/vim-indentation`,
+                    label: 'Vim Indentation',
+                    description: "All .vimrc configurations relating to indentation"
+                },
+            ]
+        }
+    ];
 </script>
 
-<p>this webpage is currently under construction</p>
+{#each sections as section}
+    <div class="wrapper-section">
+        <h2>{section.heading}</h2>
+        <div>{section.description}</div>
+        <ul>
+            {#each section.list as { url, label, description }}
+                <li>
+                    <a class="anchor-label" href={url}>
+                        {label}
+                    </a>
 
-<ul>
-    <li>
-        <a href="{base}/articles/knight-probability-in-chessboard">
-            Leet Code 688. Knight probability in chessboard
-        </a>
-    </li>
+                    <a class="anchor-character" href={url}>
+                        ⧉
+                    </a>
 
-    <li>
-        <a href="{base}/articles/gamma-functions">
-            Gamma Functions
-        </a>
-    </li>
-
-    <li>
-        <a href="{base}/articles/vim-indentation">
-            Vim Indentation Configurations
-        </a>
-    </li>
-</ul>
+                    <div class="div-description">
+                        {description}
+                    </div>
+                </li>
+            {/each}
+        </ul>
+    </div>
+{/each}
 
 <style lang="scss">
-    p {
-        color: white;
+    $white-smoke: #f5f5f5;
+
+    // resetting default margins
+    h2, ul { margin: 0 }
+
+    .wrapper-section {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        margin-bottom: 1.5rem;
     }
 
-    a {
-        color: rgb(88, 166, 255);
+    h2 {
+        color: $white-smoke;
+        font-size: 1.5rem;
+        font-family: "JetBrains Mono";
+    }
+
+    div {
+        color: $white-smoke;
+        font-size: 1rem;
+		font-family: 'Albert Sans';
+    }
+
+    ul {
+        li {
+            color: $white-smoke;
+        }
+
+        :not(li:last-child) {
+            margin-bottom: 1rem;
+        }
+    }
+
+    .anchor-label, .anchor-character {
+        color: lightblue;
+        font-size: 1.25rem;
+		font-family: 'Albert Sans';
+        text-decoration: none;
+    }
+
+    .div-description {
+        font-style: italic;
     }
 </style>
