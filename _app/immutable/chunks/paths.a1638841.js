21c9f939450f06db85b3d4d2eb4c1f1fe4d2ae54
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2024-04-13 22:28:26 +0100

    Sat 13 Apr 2024 10:28:26 PM BST

---
+++ _app/immutable/chunks/paths.a1638841.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1gznu0k)==null?void 0:s.base)??"/yalin.io";var a;const t=((a=globalThis.__sveltekit_1gznu0k)==null?void 0:a.assets)??e;export{t as a,e as b};
