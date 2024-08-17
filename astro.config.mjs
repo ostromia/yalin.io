e8790b64e605df04bd4ae99c818bee580d6172e7
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2024-08-18 00:43:13 +0100

    rewrite RepositoryCard in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +10,5 @@ export default defineConfig({
         mdx(),
         sitemap(),
         react(),
-        svelte()
     ],
 });
