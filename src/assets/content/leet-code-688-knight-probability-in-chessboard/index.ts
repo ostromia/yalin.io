4943eaf3a1af5ce901d02804aa657d9411f447c5
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-29 13:13:30 +0100

    organise article assets

---
+++ src/assets/content/leet-code-688-knight-probability-in-chessboard/index.ts
@@ -0,0 +1,13 @@
+import knight from "./knight.png";
+import knight_m0 from "./knight_m0.png";
+import knight_m1 from "./knight_m1.png";
+import knight_m2 from "./knight_m2.png";
+import knight_m3 from "./knight_m3.png";
+
+export const assets = {
+    knight,
+    knight_m0,
+    knight_m1,
+    knight_m2,
+    knight_m3
+};
