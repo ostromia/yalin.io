87da6f0c675141b954ed236d9171b1020c8f8ba6
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-17 19:43:32 +0000

    add reglup to projects

--- crawler/index.ts
+++ crawler/index.ts
@@ -27,7 +27,7 @@ async function getUserRepos(username: string, names: string[] = []) {
 
 async function main() {
     const USERNAME = "ostromia";
-    const names = ["promethium", "zygon", "dotfiles", "yalin.io"];
+    const names = ["promethium", "reglup", "dotfiles", "yalin.io"];
     const keys = ["full_name", "description", "languages"];
 
     const repositories = await getUserRepos(USERNAME, names);
@@ -40,7 +40,7 @@ async function main() {
         }
     }
 
-    const projects = ["promethium", "zygon"];
+    const projects = ["promethium", "reglup"];
 
     const result = {
         projects: repositories.filter((r: any) => projects.includes(r.full_name.split("/")[1])),
@@ -50,6 +50,7 @@ async function main() {
     for (const p of result.projects) {
         if (p.full_name == "ostromia/promethium") p.languages = ["Python"];
         if (p.full_name == "ostromia/zygon") p.languages = ["Svelte", "TypeScript", "SCSS"];
+        if (p.full_name == "ostromia/reglup") p.languages = ["TypeScript"];
     }
 
     for (const p of result.other) {
