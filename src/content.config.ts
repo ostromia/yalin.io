66ae1aa08ef462678f1214e6fccd456b0e44e6f8
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-05-28 12:31:36 +0100

    got it to work

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,43 @@
 import { defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob, type Loader } from "astro/loaders";
 import { z } from "astro/zod";
 
+// https://stackoverflow.com/questions/79243125/customizing-definecollection-in-astro
+
+const contentLoader: Loader = {
+    ...glob,
+    name: "Content Loader",
+    load: async (loaderParams) => {
+        const { store } = loaderParams;
+
+        const baseLoader = glob({
+            pattern: "**/*.{md,mdx}",
+            base: "./src/content"
+        });
+
+        await baseLoader.load.call(this, loaderParams);
+
+        let items = [...store.entries()].map(([_, value]) => value);
+
+        items = items.map((item) => {
+            const locale = item.filePath!.split(".").at(-2) || "en";
+            return {
+                ...item,
+                data: {
+                    ...item.data,
+                    locale
+                }
+            };
+        });
+        store.clear();
+        items.forEach((item) => {
+            store.set({ ...item });
+        });
+    }
+};
+
 const writing = defineCollection({
-    loader: glob({ base: "./src/content", pattern: "**/*.{md,mdx}" }),
+    loader: contentLoader,
     schema: ({ image }) =>
         z.object({
             title: z.string(),
