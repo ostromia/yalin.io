d7d6c970795cfb710aec0e1cf8e1ab0a7caa7f29
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2026-05-28 12:57:08 +0100

    clean content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,35 +2,30 @@ import { defineCollection } from "astro:content";
 import { glob, type Loader } from "astro/loaders";
 import { z } from "astro/zod";
 
-// https://stackoverflow.com/questions/79243125/customizing-definecollection-in-astro
-
 const contentLoader: Loader = {
     ...glob,
     name: "Content Loader",
-    load: async (loaderParams) => {
-        const { store } = loaderParams;
+    load: async (loaderContext) => {
+        const { store } = loaderContext;
 
-        const baseLoader = glob({
+        const globLoader = glob({
             pattern: "**/*.{md,mdx}",
             base: "./src/content"
         });
 
-        await baseLoader.load.call(this, loaderParams);
+        await globLoader.load.call(this, loaderContext);
 
-        let items = [...store.entries()].map(([_, value]) => value);
+        const result = Array.from(store.entries(), ([, value]) => ({
+            ...value,
+            data: {
+                ...value.data,
+                locale: value.filePath!.replace(/\.(md|mdx)$/, "")
+            }
+        }));
 
-        items = items.map((item) => {
-            const locale = item.filePath!.split(".").at(-2) || "en";
-            return {
-                ...item,
-                data: {
-                    ...item.data,
-                    locale
-                }
-            };
-        });
         store.clear();
-        items.forEach((item) => {
+
+        result.forEach((item) => {
             store.set({ ...item });
         });
     }
